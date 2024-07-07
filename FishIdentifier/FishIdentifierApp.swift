9952404c4c455f5ef57195379a3b126114410b5b
Derek Hessinger <derekjhessinger@gmail.com>
2024-07-07 17:38:01 -0400

    Initial Commit

---
+++ FishIdentifier/FishIdentifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  FishIdentifierApp.swift
+//  FishIdentifier
+//
+//  Created by Derek Hessinger on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct FishIdentifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
