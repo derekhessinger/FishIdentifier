4152848639a2521ed4543e05fe5f1036c7f376fa
Derek Hessinger <derekjhessinger@gmail.com>
2024-07-09 15:53:47 -0400

    updated background color and text, added ImagePickerView to homescreen

--- FishIdentifier/Views/ContentView.swift
+++ FishIdentifier/Views/ContentView.swift
@@ -1,26 +1,103 @@
-//
-//  ContentView.swift
-//  FishIdentifier
-//
-//  Created by Derek Hessinger on 7/7/24.
-//
-
 import SwiftUI
 
-struct ContentView: View{
-    var body: some View{
-        ZStack{
-            Color.blue
-                .ignoresSafeArea()
+struct ContentView: View {
+    @State private var showImagePicker = false
+    @State private var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    @State private var selectedImage: UIImage?
+
+    var body: some View {
+        ZStack {
+            Color(red: 0.2, green: 0.7, blue: 1.0) // Light blue background
+                .edgesIgnoringSafeArea(.all)
             
-            Text("Fish Identifier")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .foregroundColor(.white)
-            
+            VStack(spacing: 20) {
+                Text("FishIdentifier")
+                    .font(.system(size: 36, weight: .bold, design: .default))
+                    .italic()
+                    .padding()
+                
+                if let image = selectedImage {
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 300)
+                }
+                
+                Button(action: {
+                    self.sourceType = .camera
+                    self.showImagePicker = true
+                }) {
+                    HStack {
+                        Image(systemName: "camera")
+                        Text("Take a Picture")
+                    }
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                }
+                
+                Button(action: {
+                    self.sourceType = .photoLibrary
+                    self.showImagePicker = true
+                }) {
+                    HStack {
+                        Image(systemName: "photo.on.rectangle")
+                        Text("Select a Picture")
+                    }
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .padding()
+                    .background(Color.green)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                }
+            }
+            .padding()
+        }
+        .sheet(isPresented: $showImagePicker) {
+            ImagePicker(sourceType: self.sourceType, selectedImage: self.$selectedImage)
         }
     }
 }
+
+// Keep the ImagePicker struct as it was in the previous example
+
+struct ImagePicker: UIViewControllerRepresentable {
+    var sourceType: UIImagePickerController.SourceType
+    @Binding var selectedImage: UIImage?
+    @Environment(\.presentationMode) private var presentationMode
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = sourceType
+        imagePicker.delegate = context.coordinator
+        return imagePicker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        let parent: ImagePicker
+
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let image = info[.originalImage] as? UIImage {
+                parent.selectedImage = image
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
+
+
 
 struct ContentView_Previews: PreviewProvider{
     static var previews: some View{
---
+++ FishIdentifier/Views/ImagePickerView.swift
@@ -0,0 +1,43 @@
+//
+//  ImagePickerView.swift
+//  FishIdentifier
+//
+//  Created by Derek Hessinger on 7/9/24.
+//
+
+import SwiftUI
+
+struct ImagePickerView: UIViewControllerRepresentable {
+    @Binding var selectedImage: UIImage?
+    @Environment(\.presentationMode) var presentationMode
+    var sourceType: UIImagePickerController.SourceType = .photoLibrary
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePickerView>) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.sourceType = sourceType
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePickerView>) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: ImagePickerView
+
+        init(_ parent: ImagePickerView) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let image = info[.originalImage] as? UIImage {
+                parent.selectedImage = image
+            }
+
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
